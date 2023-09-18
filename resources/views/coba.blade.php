375028e905ffdd8b1edfbc01f08630c2340e207a
HartLianger <blyueeyes@yahoo.com>
2023-09-18 22:33:03 +0700

    Latihan membuat struktur folder view

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-compatible" content="ie=edge">
+    <title>Daftar Barang</title>
+</head>
+<body>
+    <h1>Daftar Barang</h1>
+    <o1>
+        <li>Laptop</li>
+        <li>HP</li>
+        <li>TV</li>
+    </o1>
+</body>
+</html>
