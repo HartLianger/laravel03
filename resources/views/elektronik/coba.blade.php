cd3d3383d378703fc8e164cc49a5e8de126a0875
HartLianger <blyueeyes@yahoo.com>
2023-09-18 23:18:25 +0700

    latihan kode PHP dalam view

---
+++ resources/views/elektronik/coba.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-compatible" content="ie=edge">
+    <title>Daftar Barang</title>
+</head>
+<body>
+    <h1>Daftar Barang</h1>
+    <o1>
+        <li>Laptop</li>
+        <li>HP</li>
+        <li>TV</li>
+    </o1>
+    <div>
+        Copyright  ©  <?php echo date("Y"); ?> Robby
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/coba', function () {
-    return view('coba');
+    return view('elektronik.coba');
 });
